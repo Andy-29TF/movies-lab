c08d97096a25288ee99a1279ae74b005b4d7e984
Andy-29TF <teodorandy29@gmail.com>
2021-07-15 22:53:00 +0300

    test for automatic deploy from Heroku

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 // css for max/min-width
-import './utils/utility-classes.css';
+import "./utils/utility-classes.css";
 // Router
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 // redux
-import { connect } from 'react-redux';
-import { fetchRawMovies } from './redux/actions/moviesProcessing';
-import { fetchRawNews } from './redux/actions/newsProcessing'
+import { connect } from "react-redux";
+import { fetchRawMovies } from "./redux/actions/moviesProcessing";
+import { fetchRawNews } from "./redux/actions/newsProcessing";
 // Import pages for router
-import Home from './pages/home/Home';
-import MoviesPage from './pages/moviesPage/MoviesPage';
-import TitlePage from './pages/titlePage/TitlePage'
-import News from './pages/news/News';
-import NewsPage from './pages/newsPage/NewsPage'
-import MyList from './pages/myList/MyList';
-import Page404 from './pages/page404/Page404';
+import Home from "./pages/home/Home";
+import MoviesPage from "./pages/moviesPage/MoviesPage";
+import TitlePage from "./pages/titlePage/TitlePage";
+import News from "./pages/news/News";
+import NewsPage from "./pages/newsPage/NewsPage";
+import MyList from "./pages/myList/MyList";
+import Page404 from "./pages/page404/Page404";
 //< ThemeProvider and things related to it
-import { ThemeProvider } from 'styled-components';
-import { darkTheme, lightTheme } from './utils/themes';
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./utils/themes";
 // Background
-import DarkBackground from './assets/backgroung-img/bg-dark-img.svg'
-import LightBackground from './assets/backgroung-img/bg-light-img.svg'
+import DarkBackground from "./assets/backgroung-img/bg-dark-img.svg";
+import LightBackground from "./assets/backgroung-img/bg-light-img.svg";
 // //* import styled-components
-import { AppContainer } from './app.styles';
+import { AppContainer } from "./app.styles";
 
 function App(props) {
   const { fetchRawMovies, fetchRawNews, theme } = props;
@@ -30,21 +30,23 @@ function App(props) {
   useEffect(() => {
     fetchRawMovies();
     fetchRawNews();
-  }, [fetchRawMovies, fetchRawNews])
-
+  }, [fetchRawMovies, fetchRawNews]);
 
   return (
     <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
-      <AppContainer darkBackground={DarkBackground} lightBackground={LightBackground}>
+      <AppContainer
+        darkBackground={DarkBackground}
+        lightBackground={LightBackground}
+      >
         <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/movies/:filteringRules' component={MoviesPage}/>
-          <Route exact path='/title/:id' component={TitlePage}/>
-          <Route exact path='/news-list' component={News}/>
-          <Route exact path='/news/:id' component={NewsPage}/>
-          <Route exact path='/my-list' component={MyList}/>
-          <Route path='/page-404' component={Page404} />
-          <Route path='*' component={Page404} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/movies/:filteringRules" component={MoviesPage} />
+          <Route exact path="/title/:id" component={TitlePage} />
+          <Route exact path="/news-list" component={News} />
+          <Route exact path="/news/:id" component={NewsPage} />
+          <Route exact path="/my-list" component={MyList} />
+          <Route path="/page-404" component={Page404} />
+          <Route path="*" component={Page404} />
         </Switch>
       </AppContainer>
     </ThemeProvider>
@@ -53,15 +55,19 @@ function App(props) {
 
 function mapStateToProps(state) {
   return {
-      theme: state.theme.themeType
-  }
+    theme: state.theme.themeType,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    fetchRawMovies: () => { dispatch(fetchRawMovies()) },
-    fetchRawNews: () => { dispatch(fetchRawNews()) }
-  }
+    fetchRawMovies: () => {
+      dispatch(fetchRawMovies());
+    },
+    fetchRawNews: () => {
+      dispatch(fetchRawNews());
+    },
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
